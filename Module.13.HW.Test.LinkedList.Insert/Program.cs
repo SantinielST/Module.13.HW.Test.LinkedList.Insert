730a278a2f214c767aeae6bc9c9e1d12c6ee0808
Name <email@example.ru>
2025-03-11 15:21:34 +0300

    Add project files.

---
+++ Module.13.HW.Test.LinkedList.Insert/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Module._13.HW.Test.LinkedList.Insert
+{
+    internal class Program
+    {
+        // Объявим связанный список
+        private static LinkedList<string> UsersNames = new LinkedList<string>();
+
+        static void Main(string[] args)
+        {
+            // Добавляем в с писок имена
+            UsersNames.AddFirst("Александр");
+            var igor = UsersNames.AddFirst("Игорь");
+            UsersNames.AddFirst("Виктор");
+
+            var stopWatch = Stopwatch.StartNew(); // Запускаем таймер
+
+            UsersNames.AddAfter(igor, "Павел"); // Вставляем новое имя
+
+            Console.WriteLine($"Вставка в  словарь: {stopWatch.Elapsed.TotalMilliseconds}  мс"); // Выводим результат
+        }
+    }
+}
+
